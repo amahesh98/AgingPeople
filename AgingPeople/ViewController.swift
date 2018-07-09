28b5658f1615992ff957f55b9a22a48149ab1a71
Ashwin <mahesh.ashwin1998@gmail.com>
2018-07-09 13:39:26 -0700

    Completed

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tableData=["Alex", "Ashwin", "Andrew", "Joe", "Dave", "Rod", "Ken", "Eli", "Michael Choi", "Joshua", "Tony", "Jimmy GQ"]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate=self
+        tableView.dataSource=self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +27,24 @@ class ViewController: UIViewController {
     }
 
 
+}
+extension ViewController:UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text=tableData[indexPath.row]
+        cell.detailTextLabel?.text=String(arc4random_uniform(100))
+        cell.textLabel?.textColor=UIColor.white
+        cell.detailTextLabel?.textColor=UIColor.white
+        let bgColorView=UIView()
+        bgColorView.backgroundColor=UIColor.darkGray
+        cell.selectedBackgroundView=bgColorView
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
